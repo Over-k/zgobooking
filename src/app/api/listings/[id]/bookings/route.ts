25d552eb4599298e6a77e732a34fe6ac2921376b
= <=>
2025-07-22 03:05:26 +0100

    resolve the build error

--- src/app/api/listings/[id]/bookings/route.ts
+++ src/app/api/listings/[id]/bookings/route.ts
@@ -1,10 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   try {
     const { id } = params;
     if (!id) {
